d0e05d589ad701fdb6ed189c91a9b64731fad235
woojoo0425 <shinwoojoo1995@yahoo.co.jp>
2021-11-02 14:28:30 +0900

    add Login2
    add Login2

--- app.js
+++ app.js
@@ -7,9 +7,21 @@ const HIDDEN_CLASSNAME = "hidden";
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
-    loginForm.classList.add(HIDDEN_CLASSNAME)
+    localStorage.setItem("username", username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
     greeting.innerText = "Hello " + username;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem("username");
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
