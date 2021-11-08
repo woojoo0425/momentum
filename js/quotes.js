6515e98ccccb62727deb86c71a99db3cfa5bc631
woojoo0425 <shinwoojoo1995@yahoo.co.jp>
2021-11-08 16:27:40 +0900

    add bgImage
    add bgImage

---
+++ js/quotes.js
@@ -0,0 +1,34 @@
+const quotes = [
+    {
+        quote: "All would live long, but none would be old",
+        author: "Benjamin Franklin",
+    },
+    {
+        quote: "Life is not fair, get used to it",
+        author: "Bill Gates",
+    },
+    {
+        quote: "Age is no guarantee of maturity",
+        author: "Lawana Blackwell",
+    },
+    {
+        quote: "What do you want a meaning for? Life is a desire, not a meaning",
+        author: "Charlie Chaplin",
+    },
+    {
+        quote: "It is not always the same thing to be a good man and a good citizen",
+        author: "Aristotle",
+    },
+    {
+        quote: "The gratification comes in the doing, not in the results",
+        author: "James Dean",
+    },
+]
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
